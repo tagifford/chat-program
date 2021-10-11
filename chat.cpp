6d09e7d014d65aab8b90982e4b3c74f80d480c4a
Trevor Gifford <trevoragifford@gmail.com>
2021-10-11 10:13:04 -0600

    socket structures setup following beej guide

--- chat.cpp
+++ chat.cpp
@@ -10,10 +10,61 @@
 * Member2 Login ID: XXXXXX
 ********************************************/
 
+#include <asm-generic/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+
+#define PORT "3360"
+
+
+using namespace std;
 
 int server() {
-    return 0;
+    int sockfd, new_fd;
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage client_addr;
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes = 1;
+    char s[INET_ADDRSTRLEN];
+    int rv;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        cerr << "getaddrinfo: " << gai_strerror(rv) << '\n';
+        return 1;
+    }
+
+    // loop through results and bind to first we can
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            cerr << "server: socket\n";
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            cerr << "setsockopt\n";
+            return 1;
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            cerr << "server: bind\n";
+            continue;
+        }
+        break;          //if we make it this far, we have successfully bound and can exit the loop
+    }
+
 }
 
 int main(int argc, char* argv[]){
